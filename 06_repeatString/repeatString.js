6c48d36e03e563e12a5b51ba345a58cdda857f0e
Adrian Silva <grsadrian01@outlook.com>
2025-06-07 13:48:59 -0300

    fix: solve the exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  result = [];
+  for (i = 0; i < num; i++) result.push(string);
+  return result.join("");
 };
 
 // Do not edit below this line
