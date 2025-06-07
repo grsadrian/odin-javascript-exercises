c74b0e6d7343b7ac6e3c8df8420ace1223bb116a
Adrian Silva <grsadrian01@outlook.com>
2025-06-07 14:53:58 -0300

    fix: solved the exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  args.forEach((arg) => {
+    argIndex = arr.indexOf(arg);
+    while (argIndex >= 0) {
+      arr.splice(argIndex, 1);
+      argIndex = arr.indexOf(arg);
+    }
+  });
+  return arr;
 };
 
 // Do not edit below this line
