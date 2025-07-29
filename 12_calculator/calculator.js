88916d32d06bb58b86afdd5161e0f45f45f60eb1
Adrian Silva <grsadrian01@gmail.com>
2025-07-29 19:20:36 -0300

    resolve the exercise calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sum = 0;
+  numbers.forEach((number) => {
+    sum += number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let result = 1;
+  numbers.forEach((number) => {
+    result *= number;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = x;
+
+  if (x === 0) {
+    result = 1;
+  } else {
+    for (x; x > 1; x--) {
+      result *= x - 1;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
