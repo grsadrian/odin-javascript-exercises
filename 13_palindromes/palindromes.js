672749ba208e104856d9abacbaa150b6c0713cc4
Adrian Silva <grsadrian01@gmail.com>
2025-07-29 20:54:00 -0300

    solve the exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const reverseString = require("../07_reverseString/reverseString");
 
+const palindromes = function (string) {
+  const filteredString = string
+    .toLocaleLowerCase()
+    .replaceAll(/[!,\.]/g, "")
+    .replaceAll(" ", "");
+
+  return reverseString(filteredString) === filteredString;
 };
 
 // Do not edit below this line
