b604244d52fdfb19a2f81175932e73221f75f095
Adrian Silva <grsadrian01@gmail.com>
2025-08-02 14:35:50 -0300

    solve the exercise fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (isNaN(parseInt(number)) || number < 0) {
+    return "OOPS";
+  }
 
+  let arrayNumbers = [];
+  let fibonacciSequence = [0, 1];
+  for (let i = 0; i <= number; i++) {
+    arrayNumbers.push(i);
+  }
+
+  for (let i = 2; i <= arrayNumbers.length; i++) {
+    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2]);
+  }
+
+  return fibonacciSequence[number];
 };
 
 // Do not edit below this line
