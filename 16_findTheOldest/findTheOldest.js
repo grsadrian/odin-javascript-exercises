f3ef3bf0f0ee6d09f0e5dd761f3751b6865658b7
Adrian Silva <grsadrian01@gmail.com>
2025-08-04 06:30:23 -0300

    solve the exercise find the Oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (persons) {
+  return persons.reduce((oldest, currentPerson) => {
+    const oldestAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : new Date().getFullYear() - oldest.yearOfBirth;
+    const currentAge = currentPerson.yearOfDeath
+      ? currentPerson.yearOfDeath - currentPerson.yearOfBirth
+      : new Date().getFullYear() - currentPerson.yearOfBirth;
 
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
